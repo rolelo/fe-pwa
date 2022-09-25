e23977d023b2a811999de510341d93ba964e7594
amirali26 <amiralishojaee123@gmail.com>
2022-09-25 13:48:33 +0100

    Sign out work

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -6,6 +6,72 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import {
+  Tooltip, Avatar, Menu, MenuItem,
+} from '@mui/material';
+import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import Amplify from '../../services/Amplify';
+
+const settings = ['Profile', 'Account', 'Dashboard'];
+
+const AvatarMenu: React.FC = () => {
+  const navigator = useNavigate();
+  const { mutate } = useMutation(Amplify.signOut, {
+    onSuccess: () => {
+      navigator('/');
+      toast.success('You have signed out successfully');
+    },
+    onError: (error) => {
+      toast.error(error instanceof Error ? error.message : 'Error signing you out');
+    },
+  });
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <>
+      <Tooltip title="Open settings">
+        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+          <Avatar alt="Amir" src="a" />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: '45px' }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        {settings.map((setting) => (
+          <MenuItem key={setting} onClick={handleCloseUserMenu}>
+            <Typography textAlign="center">{setting}</Typography>
+          </MenuItem>
+        ))}
+        <MenuItem key="logout" onClick={() => mutate()}>
+          <Typography textAlign="center">Logout</Typography>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
 
 export default function Navigation() {
   return (
@@ -21,10 +87,9 @@ export default function Navigation() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Box sx={{ flexGrow: 1, justifyContent: 'flex-end', textAlign: 'right' }}>
+            <AvatarMenu />
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
